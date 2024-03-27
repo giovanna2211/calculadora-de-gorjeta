835740aeb8dd5eb2b0442b8e330d321ca2759a5f
Giovanna Scalzone <gi647401@gmail.com>
2024-03-27 18:58:55 -0300

    Nono commit do CSS e Terceiro commit do JS

--- script.js
+++ script.js
@@ -29,10 +29,18 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagem)
+    botao.addEventListener("click", receberPorcentagemBotao)
 })
 
-function receberPorcentagem(evento){
-    porcentagem = Number(evento.target.value) / 100
-    console.log(porcentagem)
+function receberPorcentagemBotao(evento){
+    botoesGorjeta.forEach(botao =>{
+
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value){
+            botao.classList.add("botao-ativo")
+        }
+    })
+    porcentagem = parseFloat(evento.target.value) / 100
+
 }
